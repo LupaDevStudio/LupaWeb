72e6f22088863140aa48b5748a5ba8928e0815a5
Paul Creusy <lupa.dev.studio@gmail.com>
2024-01-07 14:19:54 +0100

    add a dynamic page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { CommonFunctionalityComponent } from './common-functionality/common-functionality.component';
+import { HttpClient } from '@angular/common/http';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,7 @@ import { CommonFunctionalityComponent } from './common-functionality/common-func
 })
 export class AppComponent extends CommonFunctionalityComponent {
   title = 'LupaWebsite';
+  lang: string = "en";
 
   // Languages parameters
   languageName: string = "En";
@@ -22,18 +26,98 @@ export class AppComponent extends CommonFunctionalityComponent {
 
   constructor(
     public translate: TranslateService,
-    public override router: Router
+    public override router: Router,
+    private titleService: Title,
+    private http: HttpClient,
   ) {
     super(router);
     translate.addLangs(['en', 'fr']);
     translate.setDefaultLang('en');
   }
 
+  override ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          // let route: ActivatedRoute = this.router.routerState.root;
+          // let routeTitle;
+          // while (route!.firstChild) {
+          //   route = route.firstChild;
+          // }
+          // if (route.snapshot.data['title']) {
+          //   routeTitle = route!.snapshot.data['title'];
+          //   if (routeTitle == "temp") {
+          //     routeTitle = route.snapshot.paramMap.get('id');
+          //   }
+          // }
+          return this.getPageTitle();
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.titleService.setTitle(`${title}`);
+        }
+      });
+  }
+
+  getPageTitle() {
+    let route: ActivatedRoute = this.router.routerState.root;
+    let routeTitle: string = "";
+    while (route!.firstChild) {
+      route = route.firstChild;
+    }
+    if (route.snapshot.data['title']) {
+      routeTitle = route!.snapshot.data['title'];
+      if (routeTitle == "temp") {
+        let temp = route.snapshot.paramMap.get('id');
+        if (temp != null) {
+          routeTitle = temp;
+        }
+      }
+    }
+    const app_content_path = "/assets/content/app-content-" + this.lang.toLowerCase() + ".json";
+    this.http.get(app_content_path).subscribe((data: any) => {
+      for (let key in data) {
+        if (key == routeTitle) {
+          routeTitle = data[key].name;
+          this.titleService.setTitle(`${routeTitle}`);
+        }
+      }
+    });
+    const news_content_path = "/assets/content/news-content-" + this.lang.toLowerCase() + ".json";
+    this.http.get(news_content_path).subscribe((data: any) => {
+      for (let key in data) {
+        if (key == routeTitle) {
+          routeTitle = data[key].name;
+          this.titleService.setTitle(`${routeTitle}`);
+        }
+      }
+    });
+    const lang_content_path = "/assets/i18n/" + this.lang.toLowerCase() + ".json";
+    this.http.get(lang_content_path).subscribe((data: any) => {
+      for (let key in data) {
+        if (key == routeTitle) {
+          routeTitle = data[key];
+          this.titleService.setTitle(`${routeTitle}`);
+        }
+      }
+    });
+    return routeTitle;
+  }
+
+  setPageTitle() {
+    let routeTitle = this.getPageTitle();
+    this.titleService.setTitle(`${routeTitle}`);
+  }
+
   switchLanguage(lang: string) {
+    this.lang = lang
     this.translate.use(lang);
     this.languageName = this.languageAssociations[lang][0];
     this.flagName = this.languageAssociations[lang][1];
     this.googlePlayImage = this.languageAssociations[lang][2];
+    this.setPageTitle();
   }
 
   reloadCurrent() {
